a83c2a695370fe6e539f35150c655c82d07a4caf
Jyoti <jg.jyotigaikwad121@gmail.com>
2025-07-05 01:42:46 +0530

    Add

---
+++ test1.java
@@ -0,0 +1,25 @@
+package com.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class test1 {
+
+
+
+	
+	   
